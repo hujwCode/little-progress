a70f5f19b269a2be6db4e7487a18a0bc799727a4
caro <caro@carodeMacBook-Air-7.local>
2026-06-05 08:15:22 +0800

    fix: use HTTP IP for DevTools compatibility

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 App({
   globalData: {
     // 修改为你的服务器地址（开发环境可用局域网IP，生产环境用域名）
-    API_BASE: 'https://little-progress.cloud',
+    API_BASE: 'http://43.153.155.195:80',
     userInfo: null,
     userId: '',
   },
